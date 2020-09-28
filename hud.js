0778a179e83f1323bf514dd52016855b28a726c0
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-09-28 16:25:24 +0900

    Update hud.js

--- hud.js
+++ hud.js
@@ -33,7 +33,7 @@ v1.0 fontsize is 12 line height/12
       hudBitmap.fillStyle = "#00ff00";
       hudBitmap.fillText('Initializing...', width / 2, height / 2);
       
-      console.log(fontsize,height)
+      console.log(hudBitmap.font,fontsize,height)
 
       // Create the camera and set the viewport to match the screen dimensions.
       var cameraHUD = new THREE.OrthographicCamera(-width/2, width/2, height/2, -height/2, 0, 3 );
