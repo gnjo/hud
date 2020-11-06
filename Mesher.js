3bb2829ba7ef48591f55b619e8e62997ff39ea47
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-11-06 18:50:04 +0900

    Update Mesher.js

--- Mesher.js
+++ Mesher.js
@@ -77,72 +77,7 @@ scene.add(b1)
    tex.needsUpdate=true////       
   }
  }
- 
- /*
-   function wall3d(url,opt,size){
-    size=size||128
-    var color='#aaaaaa'
-    //opt===0 wall
-    //opt===1 door
-    //opt===2 key
-    //opt===3 hide
-    if(opt===2) color='#aa3333'
-    if(opt===3) color='#333333'
-
-    let hash=[url,opt,size].join('_')
-    if(_tile[hash])return _tile[hash]
-    ;    
-
-    var a = new Image();
-    var c=document.createElement('canvas')
-    var ctx=c.getContext('2d')
-    var w=ctx.canvas.width=size,h=ctx.canvas.height=size
-    a.onload = function(){
-      ctx.drawImage(a,0,0,size,size)
-      if(!opt)return _tile[hash]=img
-      ;
-      ctx.globalCompositeOperation = "lighter";
-      ctx.globalAlpha=0.5
-      ctx.fillStyle = color//"#333333";
-      let w2=w*0.6,h2=h*0.8
-      ctx.fillRect(w*0.2,h*0.2,w2,h2);
-    };
-    a.src = url
-    return _tile[hash]=img
-  }  
-
- */
- 
-/*
- function icon(a,b,size,color){
-  //console.log(a,b,size,color)
-  color=color||'#ffffff'
-  //let hash=[a,b,size,color].join('_')
-  //    if(_tile[hash])return _tile[hash]
-  ;    
-  //b===0 //no outline
-  //b===1 //circle
-  //b===2 //square
-  let img=document.createElement('canvas')
-  let ctx=img.getContext('2d')
-  img.width=img.height=size||16
-  ctx.imageSmoothingEnabled= false  
-  ;
-  let r=img.width/2
-  ctx.font=`${r*1.4}px sans-serif`
-  ctx.textAlign='center'
-  ctx.textBaseline = 'middle'
-  ctx.fillStyle=color
-  ctx.fillText(a,r,r,img.width)
-  ctx.strokeStyle=color
-  ctx.lineWidth=size>16?2:1
-  if(b===1) ctx.rect(0, 0, r*2, r*2)
-  else if(b===2)ctx.arc(r, r, r-1, 0, 2*Math.PI,true)
-  ctx.stroke()
-  return img;
- }
- */
- 
+  
  function icon(a,b,size,color,rot){
   //console.log(a,b,size,color)
   color=color||'#ffffff'
